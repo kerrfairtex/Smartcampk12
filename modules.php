066eeba93b9fb43410edff3644625c3f41bef174
Kerr Fairtex <kerrfairtex@gmail.com>
2026-08-20 06:47:19 +0800

    Add modules.php routing script to support sidebar navigation links

---
+++ modules.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Router / Modules wrapper for SmartCampus K12
+ */
+session_name('SmartCampusSession');
+if ( session_status() === PHP_SESSION_NONE )
+{
+	session_start();
+}
+
+require_once 'Warehouse.php';
+
+$modname = isset($_REQUEST['modname']) ? $_REQUEST['modname'] : '';
+
+if (empty($modname)) {
+    header('Location: index.php');
+    exit;
+}
+
+// Security: basic path traversal check
+$modname = str_replace(['../', '..\\'], '', $modname);
+$filepath = 'modules/' . $modname;
+
+if (file_exists($filepath)) {
+    Warehouse('header');
+    include $filepath;
+    Warehouse('footer');
+} else {
+    echo '<div style="padding:40px;font-family:sans-serif;">';
+    echo '<h2>Module Not Found</h2>';
+    echo '<p>The requested module <code>' . htmlspecialchars($modname) . '</code> could not be found.</p>';
+    echo '<a href="index.php" style="color:#3D6FD1;text-decoration:underline;">Return to Dashboard</a>';
+    echo '</div>';
+}
